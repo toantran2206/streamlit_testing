77af18d18c55049a27fea6971eef4a8b4bb79d21
toantran2206 <toantran2206@gmail.com>
2021-09-04 09:44:08 +0700

    update
    update

--- hp_app.py
+++ hp_app.py
@@ -6,6 +6,12 @@ from pandas.io import gbq
 
 
 #Read excel file
-table1 = pd.read_excel('C:\Users\ROT2HC\Documents\GitHub\streamlit_testing\[HP] 210808-DataCracyShareDataSM.xlsx',sheet_name='DL CT')
+table1 = pd.read_excel('C:\Users\ROT2HC\Documents\GitHub\streamlit_testing\HPDataReportDataCracy(1).xlsx',sheet_name='DL CT')
 table1.head()
+
+order = table1[["Mã ĐH","Trạng thái xuất kho","Trạng thái thanh toán","Trạng thái đơn hàng"]]
+order.head()
+
+order.rename(columns={'Mã ĐH':'SalesOrderID','Trạng thái xuất kho':'StorageStatus','Trạng thái thanh toán':'PaymentStatus','Trạng thái đơn hàng':'OrderStatus'},inplace=True)
+order.head()
 
