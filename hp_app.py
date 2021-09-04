854dab4dbf24a23f1ea4b0f2851930fa4f473561
toantran2206 <toantran2206@gmail.com>
2021-09-04 09:29:51 +0700

    Update hp_app.py

--- hp_app.py
+++ hp_app.py
@@ -2,4 +2,10 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-from pandas-gbq import gbq
+from pandas.io import gbq
+
+
+#Read excel file
+table1 = pd.read_excel('C:\Users\ROT2HC\Documents\GitHub\streamlit_testing\[HP] 210808-DataCracyShareDataSM.xlsx',sheet_name='DL CT')
+table1.head()
+
